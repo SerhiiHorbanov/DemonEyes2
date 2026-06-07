3c30b61a81042626c7738bf9a319aecc774792d2
Serhiiko <serikgorbanev@gmail.com>
2026-06-07 21:34:25 +0200

    Added boids

---
+++ Assets/Scripts/Characters/Boids/Boid.cs
@@ -0,0 +1,124 @@
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+namespace Characters.Boids
+{
+	public class Boid : MonoBehaviour
+	{
+		private readonly static List<Boid> Boids = new();
+		
+		[SerializeField] private Vector2 _Velocity;
+
+		[SerializeField] private float _TargetingWeight;
+		[SerializeField] private Transform _Target;
+		
+		[SerializeField] private float _BoidForcesRadius;
+		[SerializeField] private float _CohesionWeight;
+		[SerializeField] private float _AlignmentWeight;
+		[SerializeField] private float _SeparationWeight;
+
+		[SerializeField] private float _MaxSpeed;
+		[SerializeField] private float _TargetSpeed;
+		[SerializeField] private float _SpeedAdjustmentForce;
+		
+		private float ForcesRadiusSquared => _BoidForcesRadius * _BoidForcesRadius;
+
+		private void Awake()
+		{
+			Boids.Add(this);
+		}
+
+		private void OnDestroy()
+		{
+			Boids.RemoveSwapBack(this);
+		}
+
+		private void FixedUpdate()
+		{
+			transform.position += (Vector3)(_Velocity * Time.fixedDeltaTime);
+			
+			ApplyTargeting(Time.fixedDeltaTime);
+			ApplyBoidInteractions(Time.fixedDeltaTime);
+			UpdateSpeed(Time.fixedDeltaTime);
+			UpdateRotation();
+		}
+
+		private void ApplyTargeting(float fixedDeltaTime)
+		{
+			Vector2 target = _Target.position;
+			Vector2 position = transform.position;
+			
+			Vector2 directionToTarget = (target - position).normalized;
+			_Velocity += directionToTarget * (_TargetingWeight * fixedDeltaTime);
+		}
+
+		private void UpdateRotation()
+		{
+			float desiredRotation = Mathf.Atan2(_Velocity.y, _Velocity.x) * Mathf.Rad2Deg;
+			transform.eulerAngles = new(0, 0, desiredRotation);
+		}
+
+		private void ApplyBoidInteractions(float deltaTime)
+		{
+			int boidsInRadius = 0;
+			
+			Vector2 positionsSum = Vector2.zero;
+			Vector2 separationForce = Vector2.zero;
+			Vector2 velocitiesSum = Vector2.zero;
+			
+			foreach (Boid boid in Boids)
+			{
+				Vector2 relativePosition = boid.transform.position - transform.position;
+				float distanceSquared = relativePosition.sqrMagnitude;
+				
+				if (distanceSquared > ForcesRadiusSquared)
+					continue;
+				if (boid == this)
+					continue;
+				if (distanceSquared == 0)
+					continue;
+
+				boidsInRadius++;
+				
+				positionsSum += (Vector2)boid.transform.position;
+				velocitiesSum += boid._Velocity;
+				separationForce += -relativePosition / distanceSquared * (_SeparationWeight * deltaTime);
+			}
+
+			if (boidsInRadius == 0)
+				return;
+			
+			Vector2 averagePosition = positionsSum / boidsInRadius;
+			Vector2 averageVelocity = velocitiesSum / boidsInRadius;
+
+			Vector2 directionToAveragePosition = (averagePosition - (Vector2)transform.position).normalized;
+			
+			Vector2 cohesionForce = directionToAveragePosition * (_CohesionWeight * deltaTime);
+			Vector2 alignmentForce = (averageVelocity - _Velocity) * (_AlignmentWeight * deltaTime);
+
+			_Velocity += cohesionForce + alignmentForce + separationForce;
+		}
+
+		private void UpdateSpeed(float deltaTime)
+		{
+			if (_Velocity.sqrMagnitude > _MaxSpeed * _MaxSpeed)
+			{
+				float speed = _Velocity.magnitude;
+				
+				if (speed > _MaxSpeed)
+					speed = _MaxSpeed;
+
+				if (Mathf.Abs(speed - _TargetSpeed) > _SpeedAdjustmentForce * deltaTime)
+				{
+					_Velocity = _Velocity.normalized * _TargetSpeed;
+					return;
+				}
+
+				speed += _SpeedAdjustmentForce * Mathf.Sign(_TargetSpeed - speed) * deltaTime;
+				
+				_Velocity = _Velocity.normalized * speed;
+			}
+		}
+	}
+}
